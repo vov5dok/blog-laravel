28bd3ceec45004bf8cae91b07989702b72f8b559
ivcDark <94673346+ivcDark@users.noreply.github.com>
2022-12-25 14:56:21 +0400

    Added template for blog

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,11 @@
 <?php
 
+use App\Http\Controllers\Main\IndexController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
+    Route::get('/', IndexController::class);
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
