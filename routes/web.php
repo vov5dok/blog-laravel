63bdb2e407be8899ead55f42b21018728beecdb7
ivcDark <94673346+ivcDark@users.noreply.github.com>
2022-12-26 23:05:05 +0400

    Create element in sidebar to template. Add route for view categories

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,15 @@ use App\Http\Controllers\Main\IndexController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
-    Route::get('/', IndexController::class);
+    Route::get('/', 'IndexController');
 });
 
 Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
     Route::group(['namespace' => 'Main'], function () {
-        Route::get('/', \App\Http\Controllers\Admin\Main\IndexController::class);
+        Route::get('/', 'IndexController');
+    });
+    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
+        Route::get('/', 'IndexController');
     });
 });
 
